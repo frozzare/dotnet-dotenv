c997f9f95683b8f05d7de10b2609fe5c2adab918
Fredrik Forsmo <frozzare@users.noreply.github.com>
2017-08-21 08:10:12 +0200

    Remove lot of spacing

--- tests/test-helpers/TemporaryTestFile.cs
+++ tests/test-helpers/TemporaryTestFile.cs
@@ -6,40 +6,39 @@ using Microsoft.Extensions.Configuration;
 namespace TestHelper {
 
     public class TemporaryTestFile : IDisposable {
-		const string FILENAME = "./.env";
+	const string FILENAME = "./.env";
 
-		private string _FilenameUsed;
+	private string _FilenameUsed;
 
-		public TemporaryTestFile(string body) {
-			_FilenameUsed = MakeFileName();
-			EnsureNoFile();
-			CreateNewFile(body);
+	public TemporaryTestFile(string body) {
+		_FilenameUsed = MakeFileName();
+		EnsureNoFile();
+		CreateNewFile(body);
+	}
+
+	public void Dispose() {
+		EnsureNoFile();			
+	}
+
+	public string Path { get { return _FilenameUsed; }}
+
+	private string MakeFileName() {
+		DateTime now = DateTime.Now;
+		return FILENAME + $"-{now.Millisecond}";
+	}
+
+	private void CreateNewFile(string body) {
+		using(StreamWriter writer = File.AppendText(_FilenameUsed)) {
+			writer.Write(body);
+			writer.Flush();
 		}
-
-		public void Dispose() {
-			EnsureNoFile();			
-		}
-
-		public string Path { get { return _FilenameUsed; }}
-
-		private string MakeFileName() {
-			DateTime now = DateTime.Now;
-			return FILENAME + $"-{now.Millisecond}";
-		}
-
-		private void CreateNewFile(string body) {
-			using(StreamWriter writer = File.AppendText(_FilenameUsed)) {
-				writer.Write(body);
-				writer.Flush();
-			}
-		}
+	}
         
-		private void EnsureNoFile() {
-			if (File.Exists(_FilenameUsed)) {
-				System.Console.WriteLine( $"Removing old {_FilenameUsed} file" );
-				File.Delete(_FilenameUsed);
-			}
+	private void EnsureNoFile() {
+		if (File.Exists(_FilenameUsed)) {
+			System.Console.WriteLine( $"Removing old {_FilenameUsed} file" );
+			File.Delete(_FilenameUsed);
 		}
+	}
     }
-
 }
